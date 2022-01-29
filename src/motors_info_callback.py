5722bab102a809ca30928fceb0151c048cdaba7e
Phosha <52138409+phosha@users.noreply.github.com>
2022-01-30 01:00:27 +0200

    Update motors_info_callback.py

--- src/motors_info_callback.py
+++ src/motors_info_callback.py
@@ -57,11 +57,11 @@ class Motors:
 
 
 #init motors with their angles
-motor1 = Motors(1,90) 
+motor1 = Motors(0,90) 
 rospy.loginfo(f"Motor 1 initialised with angle - {motor1.angle}, radians - {motor1.radians}, dist - {motor1.dist}")
-motor2 = Motors(2,210)
+motor2 = Motors(1,210)
 rospy.loginfo(f"Motor 2 initialised with angle - {motor2.angle}, radians - {motor2.radians}, dist - {motor2.dist}")
-motor3 = Motors(3,330)
+motor3 = Motors(2,330)
 rospy.loginfo(f"Motor 3 initialised with angle - {motor3.angle}, radians - {motor3.radians}, dist - {motor3.dist}")
 rospy.loginfo(f"Motors list {Motors.list[0]}, {Motors.list[1]}, {Motors.list[2]}")
 
