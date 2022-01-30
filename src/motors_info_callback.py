1e0d6e5f27f4f19a3a01243236b2156c0791dbfa
cyanidle <96257000+cyanidle@users.noreply.github.com>
2022-01-30 08:58:47 +0200

    Fix posting and calculating

--- src/motors_info_callback.py
+++ src/motors_info_callback.py
@@ -57,11 +57,11 @@ class Motors:
 
 
 #init motors with their angles
-motor1 = Motors(0,90) 
+motor0 = Motors(0,90) 
 rospy.loginfo(f"Motor 1 initialised with angle - {motor1.angle}, radians - {motor1.radians}, dist - {motor1.dist}")
-motor2 = Motors(1,210)
+motor1 = Motors(1,210)
 rospy.loginfo(f"Motor 2 initialised with angle - {motor2.angle}, radians - {motor2.radians}, dist - {motor2.dist}")
-motor3 = Motors(2,330)
+motor2 = Motors(2,330)
 rospy.loginfo(f"Motor 3 initialised with angle - {motor3.angle}, radians - {motor3.radians}, dist - {motor3.dist}")
 rospy.loginfo(f"Motors list {Motors.list[0]}, {Motors.list[1]}, {Motors.list[2]}")
 
@@ -90,6 +90,7 @@ odom_broadcaster = tf.TransformBroadcaster()
 
 
 while not rospy.is_shutdown():
+    Motors.updateOdom()
     current_time = rospy.Time.now()
     rospy.loginfo(f"Current time = {current_time}")
     odom_quat = tf.transformations.quaternion_from_euler(0, 0, Motors.theta)
@@ -100,7 +101,6 @@ while not rospy.is_shutdown():
         "base_link",
         "odom"
         )
-    rospy.spin()
     odom = Odometry()
     odom.header.stamp = current_time
     odom.header.frame_id = "odom"
