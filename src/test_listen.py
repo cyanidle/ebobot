fa56547e0fbe552e17ff82c51e1f3c410b9c317b
Phosha <52138409+phosha@users.noreply.github.com>
2022-01-30 18:25:38 +0200

    Update test_listen.py

--- src/test_listen.py
+++ src/test_listen.py
@@ -2,12 +2,16 @@
 import roslib; 
 import rospy
 from std_msgs.msg import String
-def callback(data):
-    rospy.loginfo(rospy.get_name()+"I heard %s",data.data)
+from std_msgs.msg import Float32MultiArray
+
+def callback(info):
+    for mot in range(0,3):
+        rospy.loginfo(f" MOTOR #{mot}: target speed {info.data[mot*4]}    current speed {info.data[(mot*4)+1]}")
+
 
 def listener():
-    rospy.init_node('listener', anonymous=True)
-    rospy.Subscriber("chatter", String, callback)
+    rospy.init_node('motors_info_callback', anonymous=True)
+    rospy.Subscriber("motors_info", Float32MultiArray, callback)
     rospy.spin()
 
 if __name__ == '__main__':
