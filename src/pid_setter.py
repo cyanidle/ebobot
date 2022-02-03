7f35a0bafc5dd3bf52f7ebe0eedf7e42c3295104
cyanidle <lyosha.doronin@gmail.com>
2022-02-03 19:10:58 +0200

    pid now is read from a text file

--- src/pid_setter.py
+++ src/pid_setter.py
@@ -1,20 +1,15 @@
 #!/usr/bin/env python3
 import rospy
 from std_msgs.msg import Float32
-##############
-coeffs = [[220,220,220],    #prop_coeff
-         [280,280,280],    #inter_coeff
-         [3,3,3]]       #diff_coeff
-
-
-#############
 rospy.init_node('pid_setter_node')
 pub = rospy.Publisher('set_pid', Float32 ,queue_size = 15)
 rospy.sleep(1)
-for coeff in coeffs:
-    for val in coeff: 
+with open('pid_settings.txt', 'r') as f:
+    pids = f.readlines()
+for coeff in pids.readlines():
+    for val in coeff.split(" "): 
         pub.publish(Float32(val))
         rospy.loginfo(f"Published {val}")
         rospy.sleep(0.1)
-
+pids.close()
 
