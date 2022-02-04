7c1bd7b7f5d3b23e582f23ebd34a76b1b3381414
cyanidle <lyosha.doronin@gmail.com>
2022-02-04 20:54:35 +0200

    1

--- src/pid_setter.py
+++ src/pid_setter.py
@@ -2,13 +2,15 @@
 import rospy
 from std_msgs.msg import Float32
 rospy.init_node('pid_setter_node')
+pid_msg = Float32()
 pub = rospy.Publisher('set_pid', Float32 ,queue_size = 15)
 rospy.sleep(1)
 with open('pid_settings.txt', 'r') as f:
     pids = f.readlines()
 for coeff in pids:
-    for val in coeff.split(" "): 
-        pub.publish(Float32(val))
+    for val in coeff.split(" "):
+        pid_msg.data = float(val) 
+        pub.publish(pid_msg)
         rospy.loginfo(f"Published {val}")
         rospy.sleep(0.1)
 pids.close()
