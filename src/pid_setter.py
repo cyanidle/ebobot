10216ef6567e175c475985cdeb6520c311f2e447
cyanidle <lyosha.doronin@gmail.com>
2022-02-04 21:07:22 +0200

    fix pid setter

--- src/pid_setter.py
+++ src/pid_setter.py
@@ -5,7 +5,7 @@ rospy.init_node('pid_setter_node')
 pid_msg = Float32()
 pub = rospy.Publisher('set_pid', Float32 ,queue_size = 15)
 rospy.sleep(1)
-with open('pid_settings.txt', 'r') as f:
+with open('$(find ebobot)/src/pid_settings.txt', 'r') as f:
     pids = f.readlines()
 for coeff in pids:
     for val in coeff.split(" "):
