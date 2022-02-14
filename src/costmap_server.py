24241a45a8547453ee8499de94cc844514f579ba
cyanidle <lyosha.doronin@gmail.com>
2022-02-14 20:02:25 +0200

    я ушел смотреть доктора хауса

--- src/costmap_server.py
+++ src/costmap_server.py
@@ -1,26 +1,42 @@
 #!/usr/bin/env python
+
 import roslib
 roslib.load_manifest('ebobot')
 import rospy
 #import math
-import cmath
+
 import tf
-import numpy as np
+
 rospy.init_node('costmap_server')
 ##########
 from nav_msgs.msg import OccupancyGrid, OccupancyGridUpdate
 from map_msgs.msg import OccupancyGridUpdate
 ###########
 import png
+from libraries.DorLib import deltaCoordsInRad
 
 
 class Costmap():
+    #Params
+    file = rospy.get_param('costmap_server/file','/src/costmap.png')
+    #/Params
+
+
+
+
+    #Global
+    list = []
+    #/Global
     file = png.Reader('src/costmap.png')
-    width,height,rows,_ = file.read()
-    
-if __name__=="__main__":
-    
-def publishUpdate(): ###An example
+    width,height,pixels,metadata = file.read()
+
+    def __init__(self):
+        Costmap.list.append(self)
+    def initCostmap(self):
+        raw_costmap = [[Costmap.pixels[x+y] for x in range(Costmap.height)] for y in range(Costmap.width)]
+
+
+    def publishUpdate(self): ###An example
         msg = OccupancyGridUpdate()
         msg.header.frame_id = "/map" ###????????
         msg.header.stamp = rospy.Time.now()
@@ -30,7 +46,7 @@ def publishUpdate(): ###An example
                 msg.data[i]
         goal.path_publisher.publish(msg)
         rospy.loginfo(f"Published new route with {len(Global.list)} points")   
-def publish(): ###An example
+    def publish(self): ###An example
         msg = OccupancyGrid()
         msg.header.frame_id = "/map" ###????????
         msg.header.stamp = rospy.Time.now()
@@ -41,4 +57,8 @@ def publish(): ###An example
                 msg.data[i]
         goal.path_publisher.publish(msg)
         rospy.loginfo(f"Published new route with {len(Global.list)} points") 
-        
+
+
+
+if __name__=="__main__":
+    
