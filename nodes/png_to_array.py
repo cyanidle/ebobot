525d29c72bcae5378bba814bc3c8b955d0c62958
GearShot77 <qwerlqwe@yandex.ru>
2022-02-17 08:22:02 +0200

    epic

---
+++ nodes/png_to_array.py
@@ -0,0 +1,16 @@
+from PIL import Image
+import numpy
+
+def converting_image_to_array():
+    image = Image.open("costmap.png")
+    array = numpy.array(image)
+    print(array)
+
+def main():
+    converting_image_to_array()
+
+if __name__ == '__main__':
+    main()
+
+
+    ## да-да это вооще не то что нужно , но я начал поПЫТКИ
