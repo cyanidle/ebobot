9836ea2f4186a7fa2421be5b85944fa3d8b5fbc0
GearShot77 <qwerlqwe@yandex.ru>
2022-02-17 14:37:05 +0200

    update

--- nodes/png_to_array.py
+++ nodes/png_to_array.py
@@ -1,13 +1,19 @@
 from PIL import Image
 import numpy
+import numpy as np
+import cv2
+
+
 
 def converting_image_to_array():
     image = Image.open("costmap.png")
     array = numpy.array(image)
-    print(array)
+
+ print(array) # что же всплыло ? 
 
 def main():
     converting_image_to_array()
+
 
 if __name__ == '__main__':
     main()
