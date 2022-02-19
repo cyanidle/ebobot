b9913e1bac9f804fe93a0539ffbf662d075c06b3
cyanidle <lyosha.doronin@gmail.com>
2022-02-19 19:59:24 +0200

    small update

--- nodes/laser_scan_callback.py
+++ nodes/laser_scan_callback.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python3
 import roslib
 roslib.load_manifest('ebobot')
+import numpy as np
 import rospy
+import tf
 from dorlib import dCoordsInRad
+rospy.init_node('laser_scan_callback')
+def robotPosCallback(odom):
+    quat = [odom.pose.pose.orientation.x,odom.pose.pose.orientation.y,odom.pose.pose.orientation.z,odom.pose.pose.orientation.w]
+    Laser.robot_pos = np.array([odom.pose.pose.position.x,odom.pose.pose.position.y,tf.transformations.euler_from_quaternion(quat)[2]]) / Laser.costmap_resolution
+class Laser:
+    #Params
+    #/Params
 
+
+
+    #Global values
+    robot_pos = np.array([0,0,0])
+    #/Global values
+    
