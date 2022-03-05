287b8237a55b46aec5ec71bd3dad472ef1a60a23
cyanidle <lyosha.doronin@gmail.com>
2022-03-05 23:12:24 +0200

    tasks wip

---
+++ nodes/task_manager.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import roslib
+roslib.load_manifest('ebobot')
+import rospy
+import yaml
+
+
+class Manager:
+    rospy.init_node("task_manager")
+    #Params
+    file = rospy.get_param("~/file", "nodes/routes/route1.yaml")
+
+    #/Params
+
+
+    #Globals
+    route = {}
+    
+
+    @classmethod
+    def read(cls):
+        with open(cls.file, "r") as stream:
+            try:
+                cls.route = (yaml.safe_load(stream))
+            except yaml.YAMLError as exc:
+                rospy.logerr(f"Loading failed ({exc})")
