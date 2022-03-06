977ea0ca7e597d9cfb4461d873d7e780d1b7efa9
cyanidle <lyosha.doronin@gmail.com>
2022-03-06 17:58:25 +0200

    task manager wip

--- nodes/task_manager.py
+++ nodes/task_manager.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
+#from multiprocessing import Condition
 import roslib
 roslib.load_manifest('ebobot')
 import rospy
 import yaml
+#from dataclasses import dataclass
+rospy.init_node("task_manager")
+#
+from markers import pubMarker
+#
 
+class Condition:
+        def __init__(self,empty = False):
 
+            pass
+        pass
 class Manager:
-    rospy.init_node("task_manager")
+    
     #Params
     file = rospy.get_param("~file", "/config/routes/route1.yaml")
 
@@ -24,3 +34,21 @@ class Manager:
                 cls.route = (yaml.safe_load(stream))
             except yaml.YAMLError as exc:
                 rospy.logerr(f"Loading failed ({exc})")
+
+    @classmethod
+    def parse(cls):
+        pass
+
+    
+
+     
+    class Task:
+        def __init__(self,num:int,actions:list, log:str = "None",condition:Condition = Condition(empty = True)):
+            pass
+
+    #@dataclass(frozen=True)
+    class Interrupt(Task):
+        pass
+
+if __name__=="__main__":
+    Manager.read()
