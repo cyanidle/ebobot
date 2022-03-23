0f9595415b0cfeaf44cb1da58cbb3e8c2f3d0c91
cyanidle <lyosha.doronin@gmail.com>
2022-03-23 14:51:51 +0200

    PAIN (arduino automatisation)

--- firmware/install.sh
+++ firmware/install.sh
@@ -1,13 +1,28 @@
+########
+port="/dev/ttyACM0"
+########
 firmware_dir=$(find ~/ -type d | grep /ebobot/firmware/main | xargs dirname)
 echo "Found firmware directory - $firmware_dir"
-cp -rf "$firmware_dir/FaBo_PWM_PCA9685" ~/Arduino/libraries
-cp -rf "$firmware_dir/LiquidCrystal_I2C_V112" ~/Arduino/libraries
-cp -rf "$firmware_dir/TimerMS" ~/Arduino/libraries
-cp -rf "$firmware_dir/main" ~/Arduino/
+arduino_dir=$(find ~/ Arduino/libraries/ | grep /Arduino/libraries | head -n 1 | xargs dirname)
+if [ -d $arduino_dir ];then
+echo "Found Arduino directory - $arduino_dir"
+cp -rf "$firmware_dir/FaBo_PWM_PCA9685" "$arduino_dir/libraries"
+cp -rf "$firmware_dir/LiquidCrystal_I2C_V112" "$arduino_dir/libraries"
+cp -rf "$firmware_dir/TimerMs" "$arduino_dir/libraries"
+cp -rf "$firmware_dir/main" "$arduino_dir/"
 echo "Libraries copied successfully"
-cd ~/Arduino/libraries
+
+cd ""$arduino_dir/libraries""
 rosrun rosserial_arduino make_libraries.py .
-cd ~/Arduino/main
-
-
-#avrdude -p m2560 -P /dev/ttyACM0 -c stk500v1 -U flash:w:$target_file
+cd ..
+if [  "$1" = "-port" ];then
+    port=$2
+    echo "Using port $port"
+    else
+    echo "Using default port ($port)"
+fi
+echo "Using sketch main in $(pwd)/main"
+arduino --board arduino:avr:mega:cpu=atmega2560 --port $port --upload main/main.ino
+else
+echo "Arduino dir not found!"
+fi
