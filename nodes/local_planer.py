743ffbe25225333002079db4e8fdf0ab8074e5ab
cyanidle <96257000+cyanidle@users.noreply.github.com>
2022-04-05 09:46:22 +0200

    Update local_planer.py

--- nodes/local_planer.py
+++ nodes/local_planer.py
@@ -260,7 +260,7 @@ class Local():
         if rad >= 0:
             return rad
         else:
-            return abs(-6.283 - rad)
+            return 6.283 + rad
     @classmethod
     def rotateAtEnd(cls):
         if cls.debug:
@@ -270,7 +270,7 @@ class Local():
         while cls.checkTurn() and not rospy.is_shutdown(): 
             diff =  (cls.getRadNorm(cls.last_target[2]) - cls.getRadNorm(cls.robot_pos[2]))
             if abs(diff) > 3.1415:
-               diff = abs(-6.283 - diff)
+               diff = -abs(6.283 - diff)
             coeff = cls.turn_coeff * abs(diff)
             if coeff > 1:
                 coeff = 1
