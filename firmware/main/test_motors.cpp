1fe45fa0419c6ec37729bfacaf010730b49eb817
cyanidle <lyosha.doronin@gmail.com>
2022-04-12 18:24:58 +0200

    -

--- firmware/main/test_motors.cpp
+++ firmware/main/test_motors.cpp
@@ -1,2 +1,33 @@
 #include "test_motors.h"
 
+pin_layout::pin_layout(ebobot::NewMotor::pin_layout _lay){
+    encoder_pin_a = _lay.encoder_pin_a;
+    encoder_pin_b = _lay.encoder_pin_b;
+}
+
+Motors::Motors(pin_layout _pin_layout, float _rad, float _ticks_per_rotation,
+ float _p, float _i, float _d,
+ float _turn_max_speed, float _max_speed, float _angle){
+    layout = _pin_layout;
+    rad = _rad; ticks_per_rotation = _ticks_per_rotation;
+    prop_coeff = _p; inter_coeff = _i; diff_coeff = _d;
+    turn_max_speed = _turn_max_speed; max_speed = _max_speed;
+    x_coeff = cos(radians(_angle)); y_coeff = sin(radians(_angle));
+    ////////////
+    num_motors++;
+    motors[num_motors] = this;
+    attachIsr(num_motors);
+ }
+
+void Motors::motorsSettingsCallback(const ebobot::NewMotor::Request &req, ebobot::NewMotor::Response &resp){
+    if (req.motor > num_motors){
+        if (req.motor - num_motors > 1) resp.resp = 1;
+        else{
+            
+            Motors new_motor{}
+
+
+        }
+    }
+    else resp.resp = 1;
+}
--- firmware/main/test_motors.h
+++ firmware/main/test_motors.h
@@ -17,45 +17,129 @@ struct pin_layout
   unit8_t pwm_pin;
   unit8_t fwd_dir_pin;
   unit8_t back_dir_pin;
-  pin_layout(ebobot::NewMoror::pin_layout);
+  pin_layout(ebobot::NewMotor::pin_layout);
 };
 class Motors{
     public:
-    Motors(pin_layout);
+    static int num_motors = 0;
+    Motors * motors[MAX_MOTORS]; 
+    Motors(pin_layout, float, float, float, float);
     static void update_all();
-    ///////////////////////
-    static void isr0(){motors_list[0]->update()};
-    static void isr1(){motors_list[1]->update()};
-    static void isr2(){motors_list[2]->update()};
-    static void isr3(){motors_list[3]->update()};
-    static void isr4(){motors_list[4]->update()};
-    static void isr5(){motors_list[5]->update()};
-    ///
+    static void speedCallback(const geometry_msgs::Twist&);
+    static void motorsSettingsCallback(const ebobot::NewMotor::Request&, ebobot::NewMotor::Response&);
+    
+    static ros::Publisher motors_info("motors_info", &motors_msg);
+    static ros::ServiceServer<ebobot::NewMotor::Request, ebobot::NewMotor::Response>
+    motors_settings_server("motors_settings_service", &motorsSettingsCallback);
+    static ros::Subscriber<geometry_msgs::Twist> speed_sub("cmd_vel", &speedCallback);
+    static ebobot::MotorsInfo motors_msg;
+    ////////////////////////
     private:
+    ///////////////////////
+    static void attachIsr(int num){
+      _pin = motors[num]->layout.encoder_pin_a;
+      switch (num)
+      {
+        case 0:
+        attachInterrupt(digitalPinToInterrupt(_pin),isr0);
+        break;
+        case 1:
+        attachInterrupt(digitalPinToInterrupt(_pin),isr1);
+        break;
+        case 2:
+        attachInterrupt(digitalPinToInterrupt(_pin),isr2);
+        break;
+        case 3:
+        attachInterrupt(digitalPinToInterrupt(_pin),isr3);
+        break;
+        case 4:
+        attachInterrupt(digitalPinToInterrupt(_pin),isr4);
+        break;
+        case 5:
+        attachInterrupt(digitalPinToInterrupt(_pin),isr5);
+        break;
+      
+      default:
+        break;
+      }
+    };
+    static void isr0(){motors_list[0]->handle()};
+    static void isr1(){motors_list[1]->handle()};
+    static void isr2(){motors_list[2]->handle()};
+    static void isr3(){motors_list[3]->handle()};
+    static void isr4(){motors_list[4]->handle()};
+    static void isr5(){motors_list[5]->handle()};
+    ///
     ///
     float prop_coeff = 300; 
     float inter_coeff = 350; 
     float diff_coeff = 3;
     volatile int X = 0; 
-    float targ_spd; 
-    float curr_spd; 
-    float last_spd;
-    float ddist; 
+    pin_layout layout;
+    float targ_spd = 0; 
+    float curr_spd = 0; 
+    float last_spd = 0;
+    int pwm = 0;
+    float ddist = 0; 
     const float absolute_max_speed; 
     const float turn_max_speed; 
     const float max_speed;
     float ticks_per_rotation;
     float rad;
-    static float radians(float);
+    static float radians(float deg){
+      return deg/360.0 * 6.283;
+    }
     float x_coeff;
     float y_coeff;
-    float inter_term;
-    float last_error;
-    void PID();
-    void update();
-    void termsReset();
-    static void speedCallback(const geometry_msgs::Twist&);
-    static void motorsSettingsCallback(const ebobot::NewMotor::Request&, ebobot::NewMotor::Response&);
+    float inter_term = 0;
+    float last_error = 0;
+    void handle(){
+      X += digitalRead(layout.encoder_pin_b)?1:-1;
+    }
+    void PID(){
+      float error = targ_spd - curr_spd;
+      inter_term += dtime * error;
+      pwm = error * prop_coeff + inter_term * inter_coeff -
+                (error - last_error) / dtime * diff_coeff;
+      inter_term = constrain(inter_term, -30000, 30000);
+      last_error = error;
+      pwm = constrain(pwm, -255, 255);
+    };
+    void update(){
+          dX = X - lastX;
+      ddist = dX * (rad / ticks_per_rotation);
+      lastX = X;
+      dist = dist + ddist;
+      curr_spd = ddist * 1000.0 / loop_delay;
+      if (stop_mot)
+      {
+        termsReset();
+        digitalWrite(layout.pwm, HIGH);
+        digitalWrite(layout.fwd_dir_pin, HIGH);
+        digitalWrite(layout.back_dir_pin, HIGH);
+      }
+
+      else
+      {
+        PID();
+        if (pwm / abs(pwm) > 0)
+        {
+          analogWrite(layout.pwm, abs(pwm));
+          digitalWrite(layout.fwd_dir_pin, HIGH);
+          digitalWrite(layout.back_dir_pin, LOW);
+        }
+        else
+        {
+          analogWrite(layout.pwm, abs(pwm)); //////////pwm varies now from -255 to 255, so we use abs
+          digitalWrite(layout.fwd_dir_pin, LOW);
+          digitalWrite(layout.back_dir_pin, HIGH);
+        }
+      }
+    };
+    void termsReset(){
+      inter_term = 0;
+      last_error = 0;
+    };
 };
 
 //#define MOTORS motors_list  
