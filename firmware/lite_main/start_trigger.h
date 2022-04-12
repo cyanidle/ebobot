2d44b2d7ccb1b43acf5b45261a05b145f76fc096
cyanidle <lyosha.doronin@gmail.com>
2022-04-12 13:45:55 +0200

    -

---
+++ firmware/lite_main/start_trigger.h
@@ -0,0 +1,44 @@
+#include <Arduino.h>
+#include <ros.h>
+#include <std_msgs/Int8.h>
+std_msgs::Int8 start_msg;
+ros::Publisher start_trigger("/ebobot/begin", &start_msg);
+bool _started = false;
+bool _route = false;
+//bool _allow_publish = false;
+const int _start_pin = 25;
+const int _switch_pin = 29;
+
+void startUpdate(){
+  //if (not _allow_publish) return;
+  if (_route){
+    if (digitalRead(_switch_pin) == HIGH){
+      _route = false;
+      start_msg.data = 1;
+      start_trigger.publish(&start_msg);
+    }
+  }
+  else{
+    if (digitalRead(_switch_pin) == LOW){
+      _route = true;
+      start_msg.data = 2;
+      start_trigger.publish(&start_msg);
+    }
+  }
+  ///////////////////////////////////////
+  if (_started){
+    if (digitalRead(_start_pin) == HIGH){
+      _started = false;
+      start_msg.data = 3;
+      start_trigger.publish(&start_msg);
+    }
+  }
+  else{
+    if (digitalRead(_start_pin) == LOW){
+      _started = true;
+      start_msg.data = 0;
+      start_trigger.publish(&start_msg);
+    }
+  }
+}
+  ///////////////////////////////////////
--- firmware/secondary/secondary.ino
+++ firmware/secondary/secondary.ino
@@ -1,46 +1,129 @@
 #include <Arduino.h>
 #include <ros.h>
+#include <ebobot/MotorsInfo.h>
+#include <std_msgs/Float32.h>
+#include <geometry_msgs/Twist.h>
 /////////////////////////
 #include "TimerMs.h"
-///////////////////////////
+//////////////////////////// наши хедеры
 #include "servos.h"
 #include "kadyrov_lcd.h"
 #include "pin_reader.h"
-////////////////////////////
-const int servo_loop_delay = 40;
-TimerMs servo_loop(servo_loop_delay, 1, 0);1
-////////////////////////////ROS init
-ros::NodeHandle_<ArduinoHardware, 8, 4, 1524, 1400> nh; // recieve/publish
-std_msgs::Float32MultiArray motors_msg;
+#include "start_trigger.h"
+////////////////////////////Все скорости в м/с
+ros::NodeHandle_<ArduinoHardware, 10, 10, 1524, 1524> nh; // recieve/publish
+
+//######################
+ebobot::MotorsInfo motors_msg;
 ros::Publisher motors_info("motors_info", &motors_msg);
+#define BAUD_RATE 115200
+//////////////////////////
+#define ENCODER_PINA0 18
+#define ENCODER_PINB0 31
+#define EN0 12
+#define FWD0 34
+#define BCK0 35
+
+#define ENCODER_PINA1 19
+#define ENCODER_PINB1 38
+#define EN1 8
+#define FWD1 37
+#define BCK1 36
+
+#define ENCODER_PINA2 3
+#define ENCODER_PINB2 49
+#define EN2 9
+#define FWD2 43
+#define BCK2 42
+///////////////////////Loop settings
+
+const int servo_loop_delay = 150;
+
+TimerMs servo_loop(servo_loop_delay, 1, 0);
+TimerMs start_loop(200, 1, 0);
+
+void debugServo(int num){
+     Servo_mot* servo = ptr_list[num];
+     char buffer[60];
+     int target;
+     sprintf(buffer, "Servo%d ch%d,min%d,max%d,spd%d,targ%d,curr%d,free%d",
+     num, servo->channel, servo->min_val ,servo->max_val, servo->speed, servo->target_state, servo->curr_val,freeRam());
+     nh.logwarn(buffer);
+}
+
 /////////////////////////////////////////////////
 void setup()
 {
+  nh.getHardware()-> setBaud(BAUD_RATE);
   nh.initNode();
-  //nh.advertise(start_trigger);
+  nh.advertise(motors_info);
+  nh.subscribe(speed_sub);
+  nh.subscribe(set_pid);
+  
   //
   nh.advertiseService(servos_server);
   nh.advertiseService(servos_settings_server);
   nh.advertiseService(lcd_server);
   nh.advertiseService(pin_reader_server);
   // Инициализация наших хедеров
+  nh.advertise(start_trigger);
+  pinMode(_start_pin, INPUT_PULLUP);
+  pinMode(_switch_pin, INPUT_PULLUP);
+  ///////////////////////////////
   lcdSetup();
   if (servosSetup())
     nh.logwarn("Servos shield found");
   else
     nh.logerror("Servos shield not found!");
+  ////////////////////////////////
+  // motors_msg.layout.data_offset = 0;
+  // motors_msg.data_length = 12;
+  // motors_msg.data = (float *)malloc(sizeof(float) * 12);
+  ///////////////////////////////
+  attachInterrupt(digitalPinToInterrupt(ENCODER_PINA0), encoder0, RISING);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_PINA1), encoder1, RISING); //Не забудь объявить (войну неграм)
+  attachInterrupt(digitalPinToInterrupt(ENCODER_PINA2), encoder2, RISING);
+  pinMode(ENCODER_PINB0, INPUT);
+  pinMode(ENCODER_PINB1, INPUT);
+  pinMode(ENCODER_PINB2, INPUT);
+  pinMode(EN0, OUTPUT);
+  pinMode(FWD0, OUTPUT);
+  pinMode(BCK0, OUTPUT);
+  pinMode(EN1, OUTPUT);
+  pinMode(FWD1, OUTPUT);
+  pinMode(BCK1, OUTPUT);
+  pinMode(EN2, OUTPUT);
+  pinMode(FWD2, OUTPUT);
+  pinMode(BCK2, OUTPUT);
+  ////////
+  //_allow_publish = true;
 }
 ////////////////////////////////
 void loop()
 {
-  if (servo_loop.tick())
-  {
-    // debugServo(0);
+  if (main_loop.tick()){
+    for (int mot = 0; mot < num_motors; mot++)
+      update_mot(mot);
+    for (int mot = 0; mot < num_motors; mot++)
+    {
+      motors_msg.num = mot;
+      motors_msg.target_speed = targ_spd[mot];
+      motors_msg.current_speed = curr_spd[mot];
+      motors_msg.ddist = ddist[mot];
+      motors_info.publish(&motors_msg);
+    }
+  }
+
+  if (servo_loop.tick()){
     servosUpdate();
-    if (not servos_debugged)
-      nh.loginfo(servos_debug);
+    if (not servos_debugged){
+      nh.logwarn(servos_debug); 
+      }
+      //debugServo(0);
     servos_debugged = true;
   }
-  motors_info.publish(&motors_msg);
+  if (start_loop.tick()){
+    startUpdate();
+  }
   nh.spinOnce();
 }
