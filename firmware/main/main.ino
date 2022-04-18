0ac2bdf36ebf9f84a85676173118fa44f37b1536
cyanidle <lyosha.doronin@gmail.com>
2022-04-18 14:49:40 +0200

    -

--- firmware/main/main.ino
+++ firmware/main/main.ino
@@ -9,7 +9,7 @@
 #include "pin_reader.h"
 #include "start_trigger.h"
 ////////////////////////////ROS init
-ros::NodeHandle_<ArduinoHardware, 10, 10, 1524, 1524> nh; // recieve/publish
+ros::NodeHandle_<ArduinoHardware, 10, 10, 1724, 1424> nh; // recieve/publish
 
 //######################
 ebobot::MotorsInfo motors_msg;
